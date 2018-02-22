0caa654e06afddeb8c0d78abdb4b07bc1fccf041
young <onlysuncolour@sina.com>
2018-02-22 17:00:23 +0800

    添加一些schema

--- server/common/schemas.js
+++ server/common/schemas.js
@@ -1,11 +1,35 @@
-var mongoose = require('mongoose');
+var Schema = require('mongoose').Schema;
 
 const schemas = {
-  todoSchema : mongoose.Schema({
+  todoSchema: Schema({
     title: String,
     id: Number,
     type: String
-  }, {collection: 'todolist'})
+  }, {collection: 'todolist'}),
+  blogSchema: Schema({
+    id: Number,
+    title: String,
+    content: String,
+    brief: String,
+    author: String,
+    author_id: String,
+    tags: Array,
+    is_hot: Boolean,
+    created_at: String,
+    updated_at: String
+  }, {collection: 'bloglist'}),
+  blogTagSchema: Schema({
+    id: Number,
+    title: String
+  }, {collection: 'blogtag'}),
+  userSchema: Schema({
+    id: Number,
+    name: Number,
+    is_admin: Boolean,
+    socket_id: String,
+    last_login_at: String,
+    created_at: String
+  }, {collection: 'userlist'})
 }
 
 module.exports = schemas
